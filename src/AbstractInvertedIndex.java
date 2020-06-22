1aed95cab6d182767afb25313fca58f97f9ee9c0
Dvir <dvir019@gmail.com>
2020-06-22 12:42:45 +0300

    Add to AbstractInvertedIndex

--- src/AbstractInvertedIndex.java
+++ src/AbstractInvertedIndex.java
@@ -1,2 +1,26 @@
-public class AbstractInvertedIndex {
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeSet;
+
+public abstract class AbstractInvertedIndex {
+    // Fields
+    HashMap<String, TreeSet<String>> map;
+
+    public AbstractInvertedIndex(){
+        map = new HashMap<>();
+    }
+
+
+    // Methods
+    public void buildInvertedIndex(File[] files) {
+        for (File file : files){
+            List<String> lines = Utils.readLines(file);
+            String fileName = file.getName();
+            for (String line : lines)
+
+        }
+    }
+
+    protected abstract void addLineToMap(String fileName, String line);
 }
