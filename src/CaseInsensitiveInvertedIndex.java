98224c0b8ab007b34aad4a1dcaadf73897b6da44
Dvir <dvir019@gmail.com>
2020-06-22 13:36:33 +0300

    Add to readLines function

--- src/CaseInsensitiveInvertedIndex.java
+++ src/CaseInsensitiveInvertedIndex.java
@@ -1,9 +1,17 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CaseInsensitiveInvertedIndex extends AbstractInvertedIndex {
     @Override
-    protected List<String> readLines(File file) {
-        List<String>
+    protected List<String> readLines(File file) {  // TODO maybe move to AbstractInvertedIndex, and add isLowercase boolean parameter
+        List<String> fileLines = Utils.readLines(file);
+        List<String> lowercaseFileLines = new ArrayList<>();
+        for (String line : fileLines) {
+            if (!isTagLine(line)) {
+                lowercaseFileLines.add(line.toLowerCase());
+            }
+        }
+        return lowercaseFileLines;
     }
 }
--- src/CaseSensitiveInvertedIndex.java
+++ src/CaseSensitiveInvertedIndex.java
@@ -1,9 +1,17 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CaseSensitiveInvertedIndex extends AbstractInvertedIndex {
     @Override
-    protected List<String> readLines(File file) {
-        return null;
+    protected List<String> readLines(File file) {  // TODO maybe move to AbstractInvertedIndex, and add isLowercase boolean parameter
+        List<String> fileLines = Utils.readLines(file);
+        List<String> lowercaseFileLines = new ArrayList<>();
+        for (String line : fileLines) {
+            if (!isTagLine(line)) {
+                lowercaseFileLines.add(line.toLowerCase());
+            }
+        }
+        return lowercaseFileLines;
     }
 }
