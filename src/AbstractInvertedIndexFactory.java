08049c52562e2840dd74c7e1d7a5dad7e1387beb
Dvir <dvir019@gmail.com>
2020-07-01 19:01:21 +0300

    Change the singleton to be static, and changed the factories

--- src/AbstractInvertedIndexFactory.java
+++ src/AbstractInvertedIndexFactory.java
@@ -1,6 +1,4 @@
 public abstract class AbstractInvertedIndexFactory {
-    // Fields
-    protected AbstractInvertedIndex invertedIndex = null;
 
     // Methods
     public abstract AbstractInvertedIndex createInvertedIndex();
--- src/CaseSensitiveFactory.java
+++ src/CaseSensitiveFactory.java
@@ -1,12 +1,6 @@
 public class CaseSensitiveFactory extends AbstractInvertedIndexFactory{
     @Override
     public AbstractInvertedIndex createInvertedIndex() {
-        if (invertedIndex == null) {
-            invertedIndex = new CaseSensitiveInvertedIndex();
-            System.out.println("New CaseSensitive index is created");
-        } else {
-            System.out.println("You already have a CaseSensitive index");
-        }
-        return invertedIndex;
+        return CaseSensitiveInvertedIndex.getInstance();
     }
 }
